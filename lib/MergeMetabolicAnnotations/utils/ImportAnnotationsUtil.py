6fb5073ff23d4aa50ec3d738c62ca74ea69ef33e
Jeff Kimbrel <jakpot@gmail.com>
2019-02-01 13:54:44 -0800

    pull annotations file from staging

--- lib/MergeMetabolicAnnotations/utils/ImportAnnotationsUtil.py
+++ lib/MergeMetabolicAnnotations/utils/ImportAnnotationsUtil.py
@@ -80,8 +80,9 @@ class ImportAnnotationsUtil:
         #     filename = self.dfu.download_staging_file(download_staging_file_params).get('copy_file_path')
 
         annotations_file_path = self.staging_dir + "/" + params.get('annotation_file')
-        filename = self.datadir + '/marinobacter.prokka.kegg.txt' # temporary fix
-        return [line.strip() for line in open(filename)]
+        #filename = self.datadir + '/marinobacter.prokka.kegg.txt' # temporary fix
+        #return [line.strip() for line in open(filename)]
+        return [line.strip() for line in open(annotations_file_path)]
 
     def annotations_to_genes(self, annotations_raw):
 
