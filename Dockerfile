43cec312f8e2ed0336b3fc506c170135465c6798
Jeff Kimbrel <jakpot@gmail.com>
2019-08-06 12:18:09 -0700

    v0.7.1
    Added CSC to compare app

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ MAINTAINER KBase Developer
 # RUN apt-get update
 
 RUN pip install bokeh
+RUN pip install pandas
 
 # -----------------------------------------
 
--- lib/MergeMetabolicAnnotations/utils/CompareAnnotationsUtil.py
+++ lib/MergeMetabolicAnnotations/utils/CompareAnnotationsUtil.py
@@ -4,10 +4,11 @@ import logging
 import json
 import uuid
 import re
+import pandas as pd
 
 from bokeh.plotting import figure, output_file
 from bokeh.io import save
-from bokeh.palettes import inferno
+from bokeh.palettes import inferno, viridis
 from bokeh.models import ColumnDataSource, FactorRange
 from bokeh.transform import factor_cmap
 from bokeh.models import HoverTool
@@ -223,7 +224,7 @@ class CompareAnnotationsUtil:
         output_html_files.append(
             {'path': output_directory,
              'name': os.path.basename(result_file_path),
-             'description': 'HTML report for compare_metabolic_annotations app'})
+             'description': 'Summary Report'})
 
         # bokeh plots
         totals_file_path = os.path.join(output_directory, 'totals.html')
@@ -234,6 +235,15 @@ class CompareAnnotationsUtil:
             {'path': output_directory,
              'name': os.path.basename(totals_file_path),
              'description': 'Ontology Totals'})
+
+        csc_file_path = os.path.join(output_directory, 'csc.html')
+        output_file(csc_file_path, title="CSC")
+        csc = self.plot_csc2(summary)
+        save(csc)
+        output_html_files.append(
+            {'path': output_directory,
+             'name': os.path.basename(csc_file_path),
+             'description': 'Cumulative Sum Plot'})
 
         # finalize html reports
         report_params = {
@@ -269,7 +279,6 @@ class CompareAnnotationsUtil:
                 'description', summary["ontology_events"][o]['method']) + '_' + str(o)
             logging.info(descriptions[o])
 
-        logging.info(descriptions)
         totals = {}
         for event in summary['ontology_events'].keys():
             totals[str(event)] = {'genes': [],
@@ -348,3 +357,97 @@ class CompareAnnotationsUtil:
         p.add_tools(HoverTool(tooltips=[("Type", "@x"), ("Count", "@counts")]))
 
         return(p)
+
+    def plot_csc2(self, summary, summary_type="rxns"):
+        descriptions = {}
+        for o in summary["ontology_events"]:
+            descriptions[o] = summary["ontology_events"][o].get(
+                'description', summary["ontology_events"][o]['method']) + '_' + str(o)
+
+        events = sorted(summary['ontology_events'].keys())
+        rxns = summary[summary_type]
+
+        # convert to sets
+        rxns_in_events = dict((int(el), set()) for el in events)
+        for rxn in rxns:
+            for event in rxns[rxn]:
+                rxns_in_events[event].add(rxn)
+
+        winning_sets = {}
+        winning_order = []
+        baseline = 0
+        df = pd.DataFrame(columns=["E", "C", "T", "L", "R"])
+        # E=event, C=comparison, T=total, L=left, R=right
+
+        for _ in range(len(rxns_in_events)):
+
+            current_right = baseline
+            current_left = baseline
+
+            # get current winner
+            longest_set_key = self.longest_set(rxns_in_events, winning_sets)
+
+            # compare current winner to all past winners
+            current = rxns_in_events[longest_set_key]
+            for past_winner in winning_order:
+                overlap = len(winning_sets[past_winner] & current)
+                current_left -= overlap
+                row = [descriptions[longest_set_key],  # E
+                       descriptions[past_winner],  # C
+                       overlap,  # T
+                       current_left,  # L
+                       current_left + overlap]  # R
+                df.loc[len(df)] = row
+                current = current - winning_sets[past_winner]
+
+            # process current winner
+            row = [descriptions[longest_set_key],  # E
+                   descriptions[longest_set_key],  # C
+                   len(current),  # T
+                   current_right,  # L
+                   current_right + len(current)]  # R
+
+            df.loc[len(df)] = row  # add to df
+            baseline += len(current)
+
+            # move current winner to past winners
+            winning_sets[longest_set_key] = rxns_in_events[longest_set_key]
+            winning_order.append(longest_set_key)
+            rxns_in_events[longest_set_key] = set()
+
+        source = ColumnDataSource(df)
+
+        type1_colormap = factor_cmap('E', palette=viridis(
+            len(df.E.unique())), factors=df.E.unique())
+        type2_colormap = factor_cmap('C', palette=viridis(
+            len(df.C.unique())), factors=df.C.unique())
+
+        p = figure(y_range=df.E.unique().tolist()[::-1],  # .tolist()[::-1] reverses the list.
+                   plot_height=300,
+                   plot_width=1000,
+                   title="Annotation events ranked by \'" + str(summary_type) + "\' contribution",
+                   tools="wheel_zoom,box_zoom,reset,save")
+
+        p.hbar(y='E',
+               height=0.9,
+               left='L',
+               right='R',
+               source=source,
+               fill_color=type2_colormap,
+               line_color="black")
+
+        p.add_tools(HoverTool(tooltips=[("Total", "@T"), ("Comparison", "@C")]))
+        p.title.text_font_size = '12pt'
+        p.xaxis.major_label_text_font_size = "12pt"
+        p.yaxis.major_label_text_font_size = "12pt"
+        return p
+
+    def longest_set(self, s, w):
+        s = s.copy()
+        for event in s:
+            for winner in w:
+                s[event] = s[event] - w[winner]
+
+        # https://stackoverflow.com/a/21839239
+        max_key, max_value = max(s.items(), key=lambda x: len(x[1]))
+        return(max_key)
