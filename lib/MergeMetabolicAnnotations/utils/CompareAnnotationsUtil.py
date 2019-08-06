a5de5e0a11e85c6de5619afb7d46de8ebbf3b723
Jeff Kimbrel <jakpot@gmail.com>
2019-08-06 11:56:48 -0700

    v0.7.0
    Compare app html plots! Just totals for now.

--- lib/MergeMetabolicAnnotations/utils/CompareAnnotationsUtil.py
+++ lib/MergeMetabolicAnnotations/utils/CompareAnnotationsUtil.py
@@ -5,6 +5,13 @@ import json
 import uuid
 import re
 
+from bokeh.plotting import figure, output_file
+from bokeh.io import save
+from bokeh.palettes import inferno
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.transform import factor_cmap
+from bokeh.models import HoverTool
+
 from installed_clients.GenomeAnnotationAPIClient import GenomeAnnotationAPI
 from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.GenomeFileUtilClient import GenomeFileUtil
@@ -218,6 +225,17 @@ class CompareAnnotationsUtil:
              'name': os.path.basename(result_file_path),
              'description': 'HTML report for compare_metabolic_annotations app'})
 
+        # bokeh plots
+        totals_file_path = os.path.join(output_directory, 'totals.html')
+        output_file(totals_file_path, title="Totals")
+        totals = self.plot_totals(summary)
+        save(totals)
+        output_html_files.append(
+            {'path': output_directory,
+             'name': os.path.basename(totals_file_path),
+             'description': 'Ontology Totals'})
+
+        # finalize html reports
         report_params = {
             'message': '',
             'html_links': output_html_files,
@@ -242,3 +260,91 @@ class CompareAnnotationsUtil:
 
         report = self.html_summary(params, summary)
         return report
+
+# plotting functions
+    def plot_totals(self, summary):
+        descriptions = {}
+        for o in summary["ontology_events"]:
+            descriptions[o] = summary["ontology_events"][o].get(
+                'description', summary["ontology_events"][o]['method']) + '_' + str(o)
+            logging.info(descriptions[o])
+
+        logging.info(descriptions)
+        totals = {}
+        for event in summary['ontology_events'].keys():
+            totals[str(event)] = {'genes': [],
+                                  'rxns': [],
+                                  'terms': []}
+
+        # genes
+        for gene in summary['genes']:
+            for term in summary['genes'][gene]['terms']:
+                for event in summary['genes'][gene]['terms'][term]:
+                    totals[str(event)]['genes'].append(gene)
+
+        # terms
+        for term in summary['terms']:
+            for event in summary['terms'][term]:
+                totals[str(event)]['terms'].append(term)
+
+        # rxns
+        for rxn in summary['rxns']:
+            for event in summary['rxns'][rxn]:
+                totals[str(event)]['rxns'].append(rxn)
+
+        # sums
+        events = []
+        types = ['genes', 'terms', 'rxns']
+
+        gene_counts = []
+        rxn_counts = []
+        term_counts = []
+
+        for event in totals:
+            logging.info(event)
+            events.append(descriptions[int(event)])
+            gene_counts.append(len(set(totals[event]['genes'])))
+            rxn_counts.append(len(set(totals[event]['rxns'])))
+            term_counts.append(len(set(totals[event]['terms'])))
+
+        data = {'events': events,
+                'genes': gene_counts,
+                'terms': term_counts,
+                'rxns': rxn_counts
+                }
+
+        x = [(event, type) for event in events for type in types]
+
+        counts = sum(zip(data['genes'], data['terms'], data['rxns']), ())
+        source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+        p = figure(y_range=FactorRange(*x),
+                   plot_height=400,
+                   plot_width=1000,
+                   title="Unique Counts per Annotation Event",
+                   tools="wheel_zoom,box_zoom,reset,save")
+
+        p.hbar(y='x',
+               right='counts',
+               height=0.9,
+               source=source,
+               line_color="black",
+               fill_color=factor_cmap('x',
+                                      palette=inferno(len(types)),
+                                      factors=types,
+                                      start=1,
+                                      end=2))
+
+        p.x_range.start = 0
+        p.y_range.range_padding = 0.1
+        p.yaxis.major_label_orientation = "horizontal"
+        p.yaxis.subgroup_label_orientation = "horizontal"
+        p.yaxis.group_label_orientation = "horizontal"
+        p.ygrid.grid_line_color = None
+        p.title.text_font_size = '12pt'
+        p.xaxis.major_label_text_font_size = "12pt"
+        p.yaxis.major_label_text_font_size = "12pt"
+        p.yaxis.group_text_font_size = "12pt"
+        p.add_tools(HoverTool(tooltips=[("Type", "@x"), ("Count", "@counts")]))
+
+        return(p)
